15e051cbce07abaaa13c2b3e06d4b30781fcb657
BT_2N2_11 <sample@bths.edu>
2023-10-30 13:52:10 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 5;
+        int b = 20;
+        while (a < 20 && b > 0) {
+            a++;
+            b--;
+            if (b > 5) {
+                b /= 2;
+            }
+        }
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
